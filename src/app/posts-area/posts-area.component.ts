1a32b6a2d281203df48affa9667e8f1ad38dd733
bzabot <bruno_zabot@outlook.com>
2024-07-11 22:41:53 +0100

    add posts.interface

--- src/app/posts-area/posts-area.component.ts
+++ src/app/posts-area/posts-area.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Post } from './posts.interface';
 
 @Component({
   selector: 'app-posts-area',
@@ -9,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './posts-area.component.css',
 })
 export class PostsAreaComponent {
-  posts = ['post1', 'post2', 'post3', 'post4'];
+  post1: Post = {
+    profileImg: 'Unknown_person.jpg',
+    userName: 'user01',
+    textContent: 'Some text about some subject',
+    subject: 'subject',
+    duration: 30,
+    date: 'Jul 11 2024,  22:40',
+  };
+
+  posts = [this.post1, this.post1, this.post1, this.post1];
 }
---
+++ src/app/posts-area/posts.interface.ts
@@ -0,0 +1,8 @@
+export interface Post {
+  profileImg: string;
+  userName: string;
+  textContent: string;
+  subject: string;
+  duration: number;
+  date: string;
+}
